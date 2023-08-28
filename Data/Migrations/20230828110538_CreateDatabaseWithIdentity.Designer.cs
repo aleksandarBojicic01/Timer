7db907dccbe817e6d9b1b6f57e37c5b8feef088d
Aleksandar Bojicic <aleksandar.bojicic01@gmail.com>
2023-08-28 13:08:32 +0200

    Finish setup

--- Data/Migrations/20230828110538_CreateDatabaseWithIdentity.Designer.cs
+++ Data/Migrations/20230828110538_CreateDatabaseWithIdentity.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Timer.Data;
 
@@ -11,9 +12,11 @@ using Timer.Data;
 namespace Timer.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230828110538_CreateDatabaseWithIdentity")]
+    partial class CreateDatabaseWithIdentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
